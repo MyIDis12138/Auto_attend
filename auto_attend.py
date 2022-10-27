5e7ff52646cf0feb360bcb513c73a79d5f97941b
Yang Gu <91805924+MyIDis12138@users.noreply.github.com>
2022-10-27 08:46:49 +0100

    Update auto_attend.py

--- auto_attend.py
+++ auto_attend.py
@@ -105,9 +105,9 @@ def main():
     print('Auto attend start.')
     while True:
         data = get_attendance_info(html_file)
-        hour = str(datetime.datetime.now(tz=pytz.timezone('Europe/London')))[11:13]:
+        hour = str(datetime.datetime.now(tz=pytz.timezone('Europe/London')))[11:13]
         for d in data:
-            if hour==d['registerstartdatetime'][11:13] and int(hour)>record_time
+            if hour==d['registerstartdatetime'][11:13] and int(hour)>record_time:
                 r = request_timetable(cfg,d)
                 record_time=int(d['registerenddatetime'][11:13])
                 print(f"attend registered: {d['activitydesc']}")
